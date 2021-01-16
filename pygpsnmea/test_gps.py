54ee85ac599116d9e36b2869cce810da763473a1
Thomas Whittam <tw.whittam@gmail.com>
2021-01-16 17:54:52 +0000

    started to add unit tests

---
+++ pygpsnmea/test_gps.py
@@ -0,0 +1,42 @@
+"""
+Unit Tests for PY GPS NMEA
+"""
+
+import unittest
+
+
+import pygpsnmea.sentences.sentence as sentence
+
+
+class NMEATests(unittest.TestCase):
+    """
+    tests related to the interpretation of GPS NMEA 0183 sentences
+    """
+
+
+    def test_correct_nmea_checksum(self):
+        """
+        feed in an NMEA 0183 sentence and calculate its checksum
+        checksum should be correct and True
+        """
+        testsentence = ('$GPRMC,152904.000,A,4611.1699,N,00117.8182,'
+                        'W,000.00,0.0,240714,,,E*46')
+        self.assertTrue(sentence.calculate_nmea_checksum(testsentence))
+
+    def test_incorrect_nmea_checksum(self):
+        """
+        feed in an NMEA 0183 sentence and calculate its checksum
+        checksum should be incorrect and False
+        """
+        testsentence = ('$GPRMC,152904.000,A,4611.1699,N,00117.8182,'
+                        'W,000.00,0.0,240714,,,E*48')
+        self.assertFalse(sentence.calculate_nmea_checksum(testsentence))
+
+
+class MiscTests(unittest.TestCase):
+    """
+    misc tests that don't fit into other catagories
+    """
+
+if __name__ == '__main__':
+    unittest.main()
