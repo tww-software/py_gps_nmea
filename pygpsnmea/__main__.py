4eb33cee286d4e23bc411d2c550ca88fc6d4e52d
Thomas Whittam <tw.whittam@gmail.com>
2021-01-18 18:44:56 +0000

    removed CLI, to focus on developing the GUI, may reintroduce at a later stage

--- pygpsnmea/__main__.py
+++ pygpsnmea/__main__.py
@@ -1,49 +1,16 @@
 """
-command line interface for PY GPS NMEA
+start PY GPS NMEA
 """
 
-
-import argparse
-
 import pygpsnmea.gui.mainwindow as gui
-import pygpsnmea.capturefile as capturefile
-import pygpsnmea.export as export
-import pygpsnmea.version as version
-
-
-def cli_arg_parser():
-    """
-    get the cli arguments and run the program
-
-    Returns:
-        parser(argparse.ArgumentParser): the command line options parser
-    """
-    desc = 'tool to decode GPS nmea sentences'
-    versiontxt = 'pygpsnmea version = {}'.format(version.VERSION)
-    parser = argparse.ArgumentParser(description=desc, epilog=versiontxt)
-    gpsoptions = parser.add_mutually_exclusive_group()
-    gpsoptions.add_argument('-i', help='input file path', dest='inputfile')
-    gpsoptions.add_argument('--gui', action='store_true', help='open the GUI')
-    return parser
 
 
 def main():
     """
-    get the command line arguments and decide what to run
+    main program code
     """
-    cliparser = cli_arg_parser()
-    cliargs = cliparser.parse_args()
-    if cliargs.inputfile:
-        sentencemanager = capturefile.open_text_file(cliargs.inputfile)
-        filestats = sentencemanager.stats()
-        filesummary = export.create_summary_text(filestats)
-        print(filesummary)
-        sentencemanager.create_kml_map('test.kml')
-        postable = sentencemanager.create_positions_table()
-        export.write_csv_file(postable, 'test.csv')
-    elif cliargs.gui:
-        maingui = gui.BasicGUI()
-        maingui.mainloop()
+    maingui = gui.BasicGUI()
+    maingui.mainloop()
 
 
 if __name__ == '__main__':
